71daefba377d89d6a6e2bb61ae9cea1f7ed4ccc9
bencatcraw <bencatcraw@gmail.com>
2023-11-27 20:27:54 -1000

    toon shader and peanut butter toast

--- Assets/_CatCafe/Scripts/ObjectSpawner.cs
+++ Assets/_CatCafe/Scripts/ObjectSpawner.cs
@@ -2,12 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using System.Linq;
 
 public class ObjectSpawner : NetworkBehaviour
 {
-    public KitchenObjectSO kitchenObject;
-    public void spawn()
+    [SerializeField] private KitchenObjectSO kitchenObject;
+    [SerializeField] private string objTag;
+    [SerializeField] private int maxOfTagged;
+    public void spawnKitchenObj()
     {
         CatCafeMultiplayer.Instance.SpawnKitchenObject(kitchenObject);
+    }
+
+    public void spawnPlateOrCup()
+    {
+        if (GameObject.FindGameObjectsWithTag(objTag).Length < maxOfTagged)
+        {
+            CatCafeMultiplayer.Instance.SpawnKitchenObject(kitchenObject);
+        }
     }
 }
